54361875298474141c7e49aa53a322e993a7c7f5
Steven_Boston <steven.cole.boston@gmail.com>
2021-07-01 03:27:27 -0700

    Route handlers lurk beneath the surface. Started on CPU

--- routeHandlers.js
+++ routeHandlers.js
@@ -1,10 +1,78 @@
 'use strict'
 
+const Character = require('./CharacterModel');
 const axios = require('axios');
 
+// //-----------------JUST JWT THINGS----------------------
+// const jwt = require('jsonwebtoken');
+// const jwksClient = require('jwks-rsa');
+// const client = jwksClient({
+//   jwksUri: 'our URI here'
+// });
+
+// //From jwt docs
+// function getKey(header, callback){
+//   client.getSigningKey(header.kid, function(err, key) {
+//     var signingKey = key.publicKey || key.rsaPublicKey;
+//     callback(null, signingKey);
+//   });
+// };
+
+
+
+//test route. Hi! 
 let test = (req,res) => {
-  console.log('connection made')
-  res.send('lets make a character')
+  console.log('test hit');
+  res.send('Welcome to the Character Sonnet Server. Head to https://charactersonnet.quest/ to make a character.')
 }
+
+//------------------------CRUD-------------------------- 
+
+// let findCharByEmail = (req,res) => {
+//   const token = req.headers.authorization.split(' ')[1];
+//   jwt.verify(token, getKey, {}, function(err, user) {
+//     if(err) {
+//       res.status(500).send('Invalid token');
+//     } else {
+//       let userEmail = user.email;
+      
+//     };
+//   });
+// }
+
+// let addChar = (req,res) => {
+//   const token = req.headers.authorization.split(' ')[1];
+//   jwt.verify(token, getKey, {}, function(err, user) {
+//     if(err) {
+//       res.status(500).send('Invalid token');
+//     } else {
+//          //we will probably need to do some formatting and 800 get requests here. maybe have that function in a  module. 
+//       const newChar = new Character ({
+//         data: 'schema details go here',
+//         email: user.email
+//       });
+//       newChar.save((err, newCharData)=> {
+//         res.send(newCharData);
+//       });
+//     }
+//   });
+// } 
+
+// let deleteChar = (req,res) => {
+//   const token = req.headers.authorization.split(' ')[1];
+//   jwt.verify(token, getKey, {}, function(err, user) {
+//     if(err) {
+//       res.status(500).send('invalid token');
+//     } else { 
+//       let charId = req.params.id;
+
+//       .deleteOne({_id: charId, email: user.email})
+//         .then(deletedCharData => {
+//           console.log(deletedBookData);
+//           res.send('this time the cat was a character, but was nonetheless deleted');
+//         });
+//     }
+//   });
+// }
 
 module.exports = {test}
